79fcec342424e5758801de821b9eefdbb8346920
Manabu Niseki <manabu.niseki@gmail.com>
2018-11-25 21:18:32 +0900

    test: add #store_newly_registered_domains spec

--- lib/ayashige/jobs/cron_job.rb
+++ lib/ayashige/jobs/cron_job.rb
@@ -4,10 +4,6 @@ module Ayashige
   module Jobs
     class CronJob
       def perform
-        store_newly_registered_domains
-      end
-
-      def store_newly_registered_domains
         Sources::WebAnalyzer.new.store_newly_registered_domains
       end
     end
