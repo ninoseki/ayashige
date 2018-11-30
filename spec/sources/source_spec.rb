8f306bbef367142a7841c817d5ae97a5f221471a
Manabu Niseki <manabu.niseki@gmail.com>
2018-11-30 19:59:41 +0900

    fix: fix typos

--- spec/sources/source_spec.rb
+++ spec/sources/source_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe Ayashige::Sources::Source, :vcr do
 
   describe "#normalize_date" do
     context "when given a valid date" do
-      it "should return a %Y-%m-%d format string when given a valid date" do
+      it "should return %Y-%m-%d format string" do
         s = subject.send(:normalize_date, "2018/01/01")
         expect(s).to eq("2018-01-01")
       end
     end
     context "when given an invalid date" do
-      it "should return a %Y-%m-%d format string when given a valid date" do
+      it "should raise an ArgumentError" do
         expect { subject.send(:normalize_date, "invalid") }.to raise_error(ArgumentError)
       end
     end
