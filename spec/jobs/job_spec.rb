1ad1d989fb9c68a14dc22e8031a7cf5b439bb512
Manabu Niseki <manabu.niseki@gmail.com>
2018-12-09 22:51:46 +0900

    fix: fix Rollbar error

---
+++ spec/jobs/job_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+RSpec.describe Ayashige::Jobs::Job do
+  subject { Ayashige::Jobs::Job.new }
+
+  describe "#perform" do
+    context "when an error is raised in #perform" do
+      before do
+        source = double("source")
+        allow(source).to receive(:store_newly_registered_domains).and_raise(ArgumentError)
+        subject.instance_variable_set(:@source, source)
+      end
+      context "when Rollbar is enabled" do
+        before do
+          allow(Ayashige::Rollbar).to receive(:available?).and_return(true)
+          allow(Ayashige::Rollbar).to receive(:error).and_return(nil)
+        end
+
+        it "should not raise any error" do
+          out = capture(:stdout) { expect { subject.perform }.not_to raise_error }
+          expect(out.empty?).to eq(true)
+        end
+      end
+      context "when Rollbar is disabled" do
+        before do
+          allow(Ayashige::Rollbar).to receive(:available?).and_return(false)
+        end
+
+        it "should not raise any error" do
+          out = capture(:stdout) { expect { subject.perform }.not_to raise_error }
+          expect(out.chomp).to eq("ArgumentError")
+        end
+      end
+    end
+  end
+end
