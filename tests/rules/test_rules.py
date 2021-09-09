df50501907e793e5e0a2c57439f76f3f61a2d697
Manabu Niseki <manabu.niseki@gmail.com>
2021-09-10 07:57:30 +0900

    feat: ignore high reputation domains
    Ignore high reputation domains from checking

--- tests/rules/test_rules.py
+++ tests/rules/test_rules.py
@@ -27,3 +27,12 @@ def test_match_rules():
     assert (
         find_rule_by_name(rules, "suspicious_keyword_levenshtein_distance") is not None
     )
+
+
+def test_match_rules_with_high_reputation_domain():
+    domain = dataclasses.Domain(
+        # it has many dashes but it has high reputation (official amazon domain)
+        "ab-pricing-console-eu-dub.dub.proxy.amazon.com"
+    )
+    rules = match_rules(domain)
+    assert len(rules) == 0
